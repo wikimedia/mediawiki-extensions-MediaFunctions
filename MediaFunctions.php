a45e30efe367d9fc2885b8a82069b7ca8127dddf
zoranzoki21 <zorandori4444@gmail.com>
2020-04-14 22:34:57 +0200

    Convert MediaFunctions to use extension registration and bump version from 1.4.0 to 1.5.0
    Bug: T204223
    Change-Id: I0d0e18244e9eae6d3d6117b92932785904f354ca

--- MediaFunctions.php
+++ MediaFunctions.php
@@ -15,31 +15,17 @@
  * @license https://www.opensource.org/licenses/BSD-2-Clause BSD-2-Clause
  */
 
-// Ensure that the script cannot be executed outside of MediaWiki
-if ( !defined( 'MEDIAWIKI' ) ) {
-    die( 'This is an extension to MediaWiki and cannot be run standalone.' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MediaFunctions' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['MediaFunctions'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['MediaFunctionsMagic'] = __DIR__ . '/MediaFunctions.i18n.magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the MediaFunctions extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the MediaFunctions extension requires MediaWiki 1.29+' );
 }
-
-// Display extension's information on "Special:Version"
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'MediaFunctions',
-	'version' => '1.4.0',
-	'author' => array(
-		'Rob Church',
-		'...'
-	),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:MediaFunctions',
-	'descriptionmsg' => 'mediafunctions-desc',
-	'license-name' => 'BSD-2-Clause'
-);
-
-// Register extension messages
-$wgMessagesDirs['MediaFunctions'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['MediaFunctionsMagic'] = __DIR__ . '/MediaFunctions.i18n.magic.php';
-
-// Load classes
-$wgAutoloadClasses['MediaFunctions'] = __DIR__ . '/src/MediaFunctions.php';
-$wgAutoloadClasses['MediaFunctionsHooks'] = __DIR__ . '/src/MediaFunctionsHooks.php';
-// Register hooks
-$wgHooks['ParserFirstCallInit'][] = 'MediaFunctionsHooks::efMediaFunctionsSetup';
